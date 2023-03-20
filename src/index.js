d0bfba2e3c859886cb56ef730b42a174b8b7b3fd
musa <khleelbasha@gmail.com>
2023-03-20 08:57:06 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Employees from './Employees';
+import Message from './Message';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import UpdateEmployee from './UpdateEmployee';
+import Upload from './Uploadfile'
+import NavBar from './Nav'
+import {BrowserRouter ,Routes,Route}  from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<App/>} ></Route>
+<Route path='/employees' element={<Employees />} ></Route>
+<Route path='/message' element={<Message />} ></Route>
+<Route path="/updateemployee" element={<UpdateEmployee />}/>
+<Route path='/upload'  element={<Upload />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
